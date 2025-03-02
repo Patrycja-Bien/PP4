f20e8db6868857bc9739b5faaa538d03324dd7ef
Bartłomiej Balsamski <bartlomiej.balsamski@gmail.com>
2025-03-02 11:55:07 +0100

    Lab 1 Ex. 3

---
+++ EShopService/EShopService/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿namespace EShopService.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Ean { get; set; } = string.Empty;
+
+        public decimal Price { get; set; }
+
+        public int Stock { get; set; } = 0;
+
+        public string Sku { get; set; } = string.Empty;
+
+        public Category Category { get; set; }
+
+        public bool Deleted { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public Guid CreatedBy { get; set; }
+
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        public Guid UpdatedBy { get; set; }
+    }
+}
