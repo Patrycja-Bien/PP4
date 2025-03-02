fa94da343a3634872dd4a2052f384bd4be87c4c5
Bartłomiej Balsamski <bartlomiej.balsamski@gmail.com>
2025-03-02 11:57:01 +0100

    Lab 1 Ex. 4

--- EShopService/EShopService/Models/Category.cs
+++ EShopService/EShopService/Models/Category.cs
@@ -1,23 +1,13 @@
 ﻿namespace EShopService.Models
 {
-    public class Product
+    public class Category
     {
         public int Id { get; set; }
 
         public string Name { get; set; } = string.Empty;
-
-        public string Ean { get; set; } = string.Empty;
-
-        public decimal Price { get; set; }
-
-        public int Stock { get; set; } = 0;
-
-        public string Sku { get; set; } = string.Empty;
-
-        public Category Category { get; set; } = default!;
-
+        
         public bool Deleted { get; set; } = false;
-
+        
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
         public Guid CreatedBy { get; set; }
