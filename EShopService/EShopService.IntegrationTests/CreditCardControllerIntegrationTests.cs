1bbf6f98c814d21f1ec41ae21ba1bd5a56d1d5d3
Patrycja-Bien <232039@student.uek.krakow.pl>
2025-04-14 12:42:35 +0200

    Lab 5 HW

---
+++ EShopService/EShopService.IntegrationTests/CreditCardControllerIntegrationTests.cs
@@ -0,0 +1,108 @@
+﻿using EShop.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+using EShop.Domain.Models;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net;
+
+namespace EShopService.IntegrationTests.Controllers;
+
+public class CreditCardControllerIntegrationTest : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+    private WebApplicationFactory<Program> _factory;
+
+    public CreditCardControllerIntegrationTest(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory
+            .WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    // pobranie dotychczasowej konfiguracji bazy danych
+                    var dbContextOptions = services
+                        .SingleOrDefault(service => service.ServiceType == typeof(DbContextOptions<DataContext>));
+
+                    //// usunięcie dotychczasowej konfiguracji bazy danych
+                    services.Remove(dbContextOptions);
+
+                    // Stworzenie nowej bazy danych
+                    services
+                        .AddDbContext<DataContext>(options => options.UseInMemoryDatabase("MyDBForTest"));
+
+                });
+            });
+
+        _client = _factory.CreateClient();
+    }
+
+    [Theory]
+    [InlineData("349779658312797")]
+    [InlineData("4024007165401778")]
+    [InlineData("345470784783010")]
+    [InlineData("4532289052809181")]
+    [InlineData("5530016454538418")]
+    [InlineData("5131208517986691")]
+    public async Task Get_CheckCard_Successful(string CardNumber)
+    {
+        // arange
+        var factory = new WebApplicationFactory<Program>();
+        HttpClient client = factory.CreateClient();
+
+        // act
+        var response = await client.GetAsync($"api/CreditCard?cardNumber={CardNumber}");
+
+        // assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Theory]
+    [InlineData("111111111111111111111111111111111111111111")]
+    [InlineData("2222222222222222222222222222222222222222222")]
+    public async Task Get_CheckCard_Unsuccessful_TooLong(string CardNumber)
+    {
+        // arange
+        var factory = new WebApplicationFactory<Program>();
+        HttpClient client = factory.CreateClient();
+
+        // act
+        var response = await client.GetAsync($"api/CreditCard?cardNumber={CardNumber}");
+
+        // assert
+        Assert.Equal(HttpStatusCode.RequestUriTooLong, response.StatusCode);
+    }
+
+    [Theory]
+    [InlineData("11111111")]
+    [InlineData("2222222")]
+    public async Task Get_CheckCard_Unsuccessful_TooShort(string CardNumber)
+    {
+        // arange
+        var factory = new WebApplicationFactory<Program>();
+        HttpClient client = factory.CreateClient();
+
+        // act
+        var response = await client.GetAsync($"api/CreditCard?cardNumber={CardNumber}");
+
+        // assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+
+    [Theory]
+    [InlineData("111111w$$$$%$#^%&*%&(*@$@11")]
+    [InlineData("222*^$%@^&^$*%&@2222")]
+    public async Task Get_CheckCard_Unsuccessful_Invalid(string CardNumber)
+    {
+        // arange
+        var factory = new WebApplicationFactory<Program>();
+        HttpClient client = factory.CreateClient();
+
+        // act
+        var response = await client.GetAsync($"api/CreditCard?cardNumber={CardNumber}");
+
+        // assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+
+}
--- EShopService/EShopService.IntegrationTests/ProductControllerIntegrationTests.cs
+++ EShopService/EShopService.IntegrationTests/ProductControllerIntegrationTests.cs
@@ -5,65 +5,66 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using EShop.Domain.Models;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace EShopService.IntegrationTests.Controllers
+namespace EShopService.IntegrationTests.Controllers;
+
+public class ProductControllerIntegrationTest : IClassFixture<WebApplicationFactory<Program>>
 {
-    public class ProductControllerIntegrationTest : IClassFixture<WebApplicationFactory<Program>>
+    private readonly HttpClient _client;
+    private WebApplicationFactory<Program> _factory;
+
+    public ProductControllerIntegrationTest(WebApplicationFactory<Program> factory)
     {
-        private readonly HttpClient _client;
-        private WebApplicationFactory<Program> _factory;
-
-        public ProductControllerIntegrationTest(WebApplicationFactory<Program> factory)
-        {
-            _factory = factory
-                .WithWebHostBuilder(builder =>
-                {
-                    builder.ConfigureServices(services =>
-                    {
-                        // pobranie dotychczasowej konfiguracji bazy danych
-                        var dbContextOptions = services
-                            .SingleOrDefault(service => service.ServiceType == typeof(DbContextOptions<DataContext>));
-
-                        //// usuniêcie dotychczasowej konfiguracji bazy danych
-                        services.Remove(dbContextOptions);
-
-                        // Stworzenie nowej bazy danych
-                        services
-                            .AddDbContext<DataContext>(options => options.UseInMemoryDatabase("MyDBForTest"));
-
-                    });
-                });
-
-            _client = _factory.CreateClient();
-        }
-
-        [Fact]
-        public async Task Get_ReturnsAllProducts()
-        {
-            // Arrange
-            using (var scope = _factory.Services.CreateScope())
+        _factory = factory
+            .WithWebHostBuilder(builder =>
             {
-                // Pobranie kontekstu bazy danych
-                var dbContext = scope.ServiceProvider.GetRequiredService<DataContext>();
+                builder.ConfigureServices(services =>
+                {
+                    // pobranie dotychczasowej konfiguracji bazy danych
+                    var dbContextOptions = services
+                        .SingleOrDefault(service => service.ServiceType == typeof(DbContextOptions<DataContext>));
 
-                dbContext.Products.RemoveRange(dbContext.Products);
+                    //// usuniêcie dotychczasowej konfiguracji bazy danych
+                    services.Remove(dbContextOptions);
 
-                // Stworzenie obiektu
-                dbContext.Products.AddRange(
-                    new Product { Name = "Product1" },
-                    new Product { Name = "Product2" }
-                );
-                // Zapisanie obiektu
-                await dbContext.SaveChangesAsync();
-            }
+                    // Stworzenie nowej bazy danych
+                    services
+                        .AddDbContext<DataContext>(options => options.UseInMemoryDatabase("MyDBForTest"));
 
-            // Act
-            var response = await _client.GetAsync("/api/product");
+                });
+            });
 
-            // Assert
-            response.EnsureSuccessStatusCode();
-            var products = await response.Content.ReadFromJsonAsync<List<Product>>();
-            Assert.Equal(2, products?.Count);
+        _client = _factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Get_ReturnsAllProducts()
+    {
+        // Arrange
+        using (var scope = _factory.Services.CreateScope())
+        {
+            // Pobranie kontekstu bazy danych
+            var dbContext = scope.ServiceProvider.GetRequiredService<DataContext>();
+
+            dbContext.Products.RemoveRange(dbContext.Products);
+
+            // Stworzenie obiektu
+            dbContext.Products.AddRange(
+                new Product { Name = "TestProduct1" },
+                new Product { Name = "TestProduct2" },
+                new Product { Name = "TestProduct3" },
+                new Product { Name = "TestProduct4" }
+            );
+            // Zapisanie obiektu
+            await dbContext.SaveChangesAsync();
         }
 
+        // Act
+        var response = await _client.GetAsync("/api/product");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var products = await response.Content.ReadFromJsonAsync<List<Product>>();
+        Assert.Equal(4, products?.Count);
     }
+
 }
